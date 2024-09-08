90cfe207977f57245832818365b37fa4d23c924f
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-09-08 20:37:57 +1000

    Tidy up player controller

--- code/PlayerControllerComponent.cs
+++ code/PlayerControllerComponent.cs
@@ -8,11 +8,12 @@ using System.Threading.Tasks;
 
 public sealed class PlayerControllerComponent : Component
 {
-	[Property] public float JumpHeight { get; set; } = 256;
+	[Property, Category("Jumping")] public float JumpHeight { get; set; } = 256;
 
-	[Property] float JumpTimeToPeak { get; set; }
+	[Property, Category( "Jumping" )] float JumpTimeToPeak { get; set; }
 
-	[Property] float JumpTimeToDescent { get; set; }
+	[Property, Category( "Jumping" )] float JumpTimeToDescent { get; set; }
+
 	[Property] CitizenAnimationHelper AnimHelper { get; set; }
 
 	[Property] LevelComponent Level { get; set; }
@@ -20,6 +21,8 @@ public sealed class PlayerControllerComponent : Component
 	[Property] public BoxCollider DeathColl { get; set; }
 
 	[Property] ScoreComp Score { get; set; }
+
+	[RequireComponent] CharacterController CharController { get; set; } 
 
 	bool Rolling = false;
 	bool Moving = false;
@@ -30,9 +33,12 @@ public sealed class PlayerControllerComponent : Component
 	float JumpGravity { get; set; }
 	float FallGravity { get; set; }
 
-	[Property] float SideMoveTime { get; set; }
-	float SideMoveTargetVel { get; set; }
+	float Gravity { get { return CharController.Velocity.y >= 0.0f ? JumpGravity : FallGravity; } }
 
+	[Property] float SideMoveTime { get; set; }
+
+
+	float SideMoveTargetVel { get; set; }
 	float SideMoveVel { get; set; }
 
 
@@ -46,22 +52,17 @@ public sealed class PlayerControllerComponent : Component
 
 	protected override void OnEnabled()
 	{
-		JumpVelocity = (2.0f * JumpHeight) / JumpTimeToPeak;
-		JumpGravity = (-2.0f * JumpHeight) / (JumpTimeToPeak * JumpTimeToPeak);
-		FallGravity = (-2.0f * JumpHeight) / (JumpTimeToDescent * JumpTimeToDescent);
+		CalculateJumpHeight();
 
 		SideMoveTargetVel = (122.0f) / SideMoveTime;
 	}
 
 	protected override void OnUpdate()
 	{
-
-		var cc = GameObject.Components.Get<CharacterController>();
-
 		if ( MovementMult == 0.0f )
 			return;
 		AnimHelper.WithVelocity( Transform.Rotation.Forward * 300.0f + Transform.Rotation.Left * SideMoveVel );
-		AnimHelper.IsGrounded = cc.IsOnGround;
+		AnimHelper.IsGrounded = CharController.IsOnGround;
 		AnimHelper.SpecialMove = MoveStyle;
 	}
 
@@ -72,18 +73,14 @@ public sealed class PlayerControllerComponent : Component
 		if ( MovementMult == 0.0f )
 			return;
 
-
-		var cc = GameObject.Components.Get<CharacterController>();
-
-
 		if ( Rolling )
 		{
-			cc.Height = 22;
+			CharController.Height = 22;
 			DeathColl.Scale = new Vector3( 48, 48, 23 );
 		}
 		else
 		{
-			cc.Height = 72;
+			CharController.Height = 72;
 			DeathColl.Scale = new Vector3( 48, 48, 74 );
 		}
 
@@ -99,9 +96,9 @@ public sealed class PlayerControllerComponent : Component
 			QueueMovement( -1 );
 		}
 
-		cc.Accelerate( Vector3.Forward * 500.0f * Score.GetSpeedMult() * GameGlobals.SpeedMultiplier );
-		cc.Velocity = cc.Velocity.WithY( SideMoveVel );
-		cc.ApplyFriction( 2.0f );
+		CharController.Accelerate( Vector3.Forward * 500.0f * Score.GetSpeedMult() * GameGlobals.SpeedMultiplier );
+		CharController.Velocity = CharController.Velocity.WithY( SideMoveVel );
+		CharController.ApplyFriction( 2.0f );
 
 
 		if (Movements.Count > 0 && !Moving)
@@ -110,34 +107,34 @@ public sealed class PlayerControllerComponent : Component
 		}
 
 
-		if ( cc.IsOnGround && (Input.Down( "Forward" ) || Input.Down( "Jump" )))
+		if ( CharController.IsOnGround && (Input.Down( "Forward" ) || Input.Down( "Jump" )))
 		{
 			float flGroundFactor = 1.0f;
 			float flMul = JumpVelocity * GameGlobals.JumpMultiplier;
 			//if ( Duck.IsActive )
 			//	flMul *= 0.8f;
 
-			cc.Punch( Vector3.Up * flMul * flGroundFactor );
+			CharController.Punch( Vector3.Up * flMul * flGroundFactor );
 			//	cc.IsOnGround = false;
 		}
 
-		cc.Move();
+		CharController.Move();
 
-		if ( !cc.IsOnGround )
+		if ( !CharController.IsOnGround )
 		{
 			var gravMult = 1.0f;
 			if ( Input.Down( "Backward" ) )
 				gravMult = 4.0f;
 
-			cc.Velocity -= Vector3.Down * GetGravity(cc) * gravMult * Time.Delta * 0.5f;
+			CharController.Velocity -= Vector3.Down * Gravity * gravMult * Time.Delta * 0.5f;
 			RollGroundTime = 0;
 		}
 		else
 		{
-			cc.Velocity = cc.Velocity.WithZ( 0 );
+			CharController.Velocity = CharController.Velocity.WithZ( 0 );
 		}
 
-		if (Input.Down("Backward") && cc.IsOnGround)
+		if (Input.Down("Backward") && CharController.IsOnGround)
 		{
 			_ = Duck();
 		}
@@ -147,7 +144,7 @@ public sealed class PlayerControllerComponent : Component
 					WrapX();
 				}*/
 
-		RunPartcles.Enabled = cc.IsOnGround;
+		RunPartcles.Enabled = CharController.IsOnGround;
 
 		if (!Moving)
 		{
@@ -191,21 +188,16 @@ public sealed class PlayerControllerComponent : Component
 		Movements.Add( dir );
 	}
 
-	float GetGravity(CharacterController cc)
-	{
-		if ( cc.Velocity.y >= 0.0f )
-		{
-			return JumpGravity;
-		}
-		else
-		{
-			return FallGravity;
-		}
-	}
-
 	public void StartMovement()
 	{
 		MovementMult = 1;
 		Score.StartScore();
+	}
+
+	void CalculateJumpHeight()
+	{
+		JumpVelocity = (2.0f * JumpHeight) / JumpTimeToPeak;
+		JumpGravity = (-2.0f * JumpHeight) / (JumpTimeToPeak * JumpTimeToPeak);
+		FallGravity = (-2.0f * JumpHeight) / (JumpTimeToDescent * JumpTimeToDescent);
 	}
 }
